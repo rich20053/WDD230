29e6ebd8113a22af031e510636ff6da681c1ee4d
rich20053 <markwrichmond@byui.edu>
2022-01-08 16:21:25 -0700

    Clean Up

--- JavaScript/last-modified.js
+++ JavaScript/last-modified.js
@@ -3,8 +3,3 @@ let strLMD = "Last Updated: " + lastModDate.getMonth()+1 + "/" + lastModDate.get
            + lastModDate.getFullYear() + " " + lastModDate.getHours() + ":" 
            + lastModDate.getMinutes() + ":" + lastModDate.getSeconds();
 document.getElementById("datemodified").textContent = strLMD;
-/*
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById("datemodified").textContent = "Last Updated: " + new Date().toLocaleDateString('en-US', options);
-*/
-
