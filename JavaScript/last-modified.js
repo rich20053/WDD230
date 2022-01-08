449e77c60f624546cc2814e96c0f2e3e0ececeb0
rich20053 <markwrichmond@byui.edu>
2022-01-08 12:24:00 -0700

    First pass reduced

--- JavaScript/last-modified.js
+++ JavaScript/last-modified.js
@@ -1,10 +1,10 @@
-/* const lastModDate = new Date(document.lastModified);
+const lastModDate = new Date(document.lastModified);
 let strLMD = "Last Updated: " + lastModDate.getMonth()+1 + "/" + lastModDate.getDate() + "/" 
            + lastModDate.getFullYear() + " " + lastModDate.getHours() + ":" 
            + lastModDate.getMinutes() + ":" + lastModDate.getSeconds();
 document.getElementById("datemodified").textContent = strLMD;
-*/
+/*
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById("datemodified").textContent = "Last Updated: " + new Date().toLocaleDateString('en-US', options);
-
+*/
 
