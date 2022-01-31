9b6fa4a5c3dbf52691d64beae9ad77be7dac9a5b
rich20053 <markwrichmond@byui.edu>
2022-01-30 20:38:14 -0700

    Week 5 V6

--- chamber/js/todays-date.js
+++ chamber/js/todays-date.js
@@ -1,2 +1,2 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById("chamberdate").textContent = new Date().toLocaleDateString('en-US', options);
+document.getElementById("chamberdate").textContent = new Date().toLocaleDateString('en-UK', options);
--- javascript/todays-date.js
+++ javascript/todays-date.js
@@ -1,3 +1,3 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById("todaysdate").textContent = new Date().toLocaleDateString('en-US', options);
-document.getElementById("chamberdate").textContent = new Date().toLocaleDateString('en-US', options);
+document.getElementById("chamberdate").textContent = new Date().toLocaleDateString('en-UK', options);
