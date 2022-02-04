75dd16da58727fb1539463fe51eb49a4580aa95d
rich20053 <markwrichmond@byui.edu>
2022-02-04 16:52:39 -0700

    Week 5 V12

--- lesson5/js/bom.js
+++ lesson5/js/bom.js
@@ -1,19 +1,20 @@
 const favChapter = document.querySelector('#favchap');
 const addButton = document.querySelector('button');
-//const delButton = document.querySelector('li button');
 const chapterList = document.querySelector('.list');
 
 addButton.addEventListener('click', () => {
     let chapterName = favChapter.value; 
-    let li = document.createElement('li');
-    li.innerHTML = chapterName + "<button>X</button>";
-    chapterList.appendChild(li);
+    if (chapterName != "") {
+        let li = document.createElement('li');
+        li.textContent = chapterName;
+        const delButton = document.createElement("button");
+        delButton.textContent = "❌";
+        li.append(delButton);
+        chapterList.append(li);    
+        delButton.addEventListener('click', () => {
+            chapterList.removeChild(li);
+            input.focus;
+        })
+    }
 })
 
-//delButton.addEventListener('click', () => {
-    //let chapterName = favChapter.value; 
-  //  let delli = document.('li');
-    //li.innerHTML = chapterName + "<button>X</button>";
-    //chapterList.appendChild(li);
-//})
-// class="deletebutton"
