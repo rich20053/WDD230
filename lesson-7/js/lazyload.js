a79d515824e1eec752ce0b2d72448e07d0d10dde
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-02-16 19:15:09 -0700

    Week 7 V1

---
+++ lesson-7/js/lazyload.js
@@ -0,0 +1,32 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+function loadImage(image) {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+      image.removeAttribute('data-src');
+    }; 
+}; 
+
+const imageOptions = {
+    threshold: 0,
+    rootMargin: "-10px"
+}
+
+const imageObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            loadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imageOptions)
+
+imagesToLoad.forEach(image => {
+    imageObserver.observe(image);
+});
+
+
+  
+  
