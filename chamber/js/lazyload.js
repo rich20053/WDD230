04a6e434d62a82b5709f3e2758fcdf8887f0eb50
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-02-17 23:15:56 -0700

    Week 7 V11

--- chamber/js/lazyload.js
+++ chamber/js/lazyload.js
@@ -9,7 +9,7 @@ function loadImage(image) {
 
 const imageOptions = {
     threshold: 0,
-    rootMargin: "-1px"
+    rootMargin: "10px" 
 }
 
 const imageObserver = new IntersectionObserver((entries, imgObserver) => {
