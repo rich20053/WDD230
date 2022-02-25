f210c9d1223779ef772b2c7a7ca934e1b365fef4
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-02-25 12:52:37 -0700

    Week 8 V10

--- chamber/js/todays-date.js
+++ chamber/js/todays-date.js
@@ -1,9 +1,6 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-/*document.getElementById("todaysdate").textContent = new Date().toLocaleDateString('en-US', options);*/
+document.getElementById("todaysdate").textContent = new Date().toLocaleDateString('en-US', options);
 document.getElementById("chamberdate").textContent = new Date().toLocaleDateString('en-UK', options);
-/*const jdate = new Date();*/
 var jdatestr = new Date().toISOString();
-console.log(jdatestr);
 jdatestr = jdatestr.substring(0,16);
 document.getElementById("joindate").value = jdatestr;
-console.log(jdatestr);
