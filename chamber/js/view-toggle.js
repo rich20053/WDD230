8b9983f655fc49eaffc77e580774771c2cc88744
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-03-04 23:03:23 -0700

    Week 9 V13

--- chamber/js/view-toggle.js
+++ chamber/js/view-toggle.js
@@ -1,7 +1,17 @@
+var viewtype = "O";
+var ww = window.innerWidth;
+
+if (ww >= 592 && ww <= 991) {
+    toggleView(); 
+    viewtype = "M";
+}
+
+/*window.addEventListener("resize", checkWidth);*/
+    
 function toggleView() {
     document.getElementById('cardviewBtn').classList.toggle("open");
     document.getElementById('listviewBtn').classList.toggle("open");
-   document.getElementById('bizdir').classList.toggle("open");    
+    document.getElementById('bizdir').classList.toggle("open");    
 }
 
 const lv = document.getElementById('listviewBtn');
@@ -9,4 +19,19 @@ lv.onclick = toggleView;
 
 const c = document.getElementById('cardviewBtn');
 c.onclick = toggleView;
+
+function checkWidth() {
+    ww = window.innerWidth;
+    if (ww >= 592 && ww <= 991) {
+        if (viewtype != "M") {
+            toggleView(); 
+            viewtype = "M";
+        }
+    } else {
+        if (viewtype == "M") {
+            toggleView(); 
+            viewtype = "O";
+        }
+    }
+}
 
