fca6c05026e48f251e22b9935830e3b1f2dc1192
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-03-11 00:04:57 -0700

    Week 11 V1

--- chamber/js/view-toggle.js
+++ chamber/js/view-toggle.js
@@ -1,13 +1,9 @@
-var viewtype = "O";
 var ww = window.innerWidth;
 
 if (ww >= 592 && ww <= 991) {
     toggleView(); 
-    viewtype = "M";
 }
 
-/*window.addEventListener("resize", checkWidth);*/
-    
 function toggleView() {
     document.getElementById('cardviewBtn').classList.toggle("open");
     document.getElementById('listviewBtn').classList.toggle("open");
@@ -20,18 +16,4 @@ lv.onclick = toggleView;
 const c = document.getElementById('cardviewBtn');
 c.onclick = toggleView;
 
-function checkWidth() {
-    ww = window.innerWidth;
-    if (ww >= 592 && ww <= 991) {
-        if (viewtype != "M") {
-            toggleView(); 
-            viewtype = "M";
-        }
-    } else {
-        if (viewtype == "M") {
-            toggleView(); 
-            viewtype = "O";
-        }
-    }
-}
 
