395b1ce3b668f6a50ebe25732c9d770235a49fd0
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-03-22 22:29:58 -0600

    TempleInn V2

---
+++ templeinn&suites/js/todays-date.js
@@ -0,0 +1,4 @@
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+/*document.getElementById("todaysdate").textContent = new Date().toLocaleDateString('en-US', options); */
+document.getElementById("headerdate").textContent = new Date().toLocaleDateString('en-UK', options);
+
